7d59f2f7edc70618f4c8866284b791d7891f3597
Benjamin Toueg <btoueg@gmail.com>
2011-04-18 00:06:23 +0200

    xsm parser DFS now yields node as it reads them.

--- lcmviewer/parser/xsm_test.py
+++ lcmviewer/parser/xsm_test.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# author : Benjamin Toueg
-# date : 17/04/11
+# This snippet is used to output an XSM file in the console
+# Copyright (C) 2011  Benjamin Toueg
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from os.path import abspath
 from sys import platform
 from array import array
 
-file_path = "../../example/XsmMultiCompoV4"
-
-CHAR_LENGTH_WORD = 8
-BYTE_LENGTH_INTEGER = 2
-BYTE_STEP_INTEGER = 4
 BYTE_STEP = 8
 
 class FileXsm:
@@ -33,6 +42,24 @@ class FileXsm:
 
   def close(self):
     self.file.close()
+
+  def depth_first_search(self,node_list):
+    for node in node_list:
+      node.load(self)
+      yield node
+      if node.type == 10:
+        for index,list_item in enumerate(node.children):
+          list_item.load(self)
+          if(list_item.type==0):
+            if(len(list_item.children))>0:
+              list_item.name = "%08d"%(index+1)
+            else:
+              list_item.name = "empty"
+        for y in self.depth_first_search(node.children):
+          yield y
+      elif node.type in [0]:
+        for y in self.depth_first_search(node.children):
+          yield y
 
   def seek(self,cursor):
     self.file.seek(cursor*BYTE_STEP)
@@ -86,6 +113,9 @@ class Node:
     self.loaded_in_memory = False
   
   def __str__(self):
+    """
+    Convert a node into a description string
+    """
     s  = "Node '%s'"%(self.name)
     s += "\nOffset : %d"%(self.offset*BYTE_STEP)
     s += "\nType : %d"%(self.type)
@@ -98,9 +128,12 @@ class Node:
         s += "\nNumber of children : %d"%(len(self.children))
       elif self.type == 10:
         pass
-      elif self.type in [1,2,3]:
+      elif self.type in [1,2]:
         s += "\nData :"
         s += ",".join([ str(item) for item in self.data])
+      elif self.type == 3:
+        s += "\nData :"
+        s += self.data
     else:
       s += "\nNot yet loaded in memory."
     return s
@@ -137,65 +170,36 @@ class Node:
     elif self.type == 2:
       self.data = file_xsm.read_reals(self.length)
     elif self.type == 3:
-      self.data = [file_xsm.read_4c() for n in xrange(self.length)]
+      self.data = ""
+      for n in xrange(self.length):
+        self.data += file_xsm.read_4c()
     else:
       print self.type
       raise
     self.loaded_in_memory = True
 
-def depth_first_search(file_xsm,node_list):
-  for node in node_list:
-    node.load(file_xsm)
-    print node
-    if node.type == 10:
-      for index,list_item in enumerate(node.children):
-        list_item.load(file_xsm)
-        if(list_item.type==0):
-          if(len(list_item.children))>0:
-            list_item.name = "%08d"%(index+1)
-          else:
-            list_item.name = "empty"
-      depth_first_search(file_xsm,node.children)
-    elif node.type in [0]:
-      depth_first_search(file_xsm,node.children)
-
-if __name__=="__main__":
+def xsmToElementList(filePath):
   file_xsm = FileXsm(file_path)
-  print file_xsm.read_4c()
+  if file_xsm.read_4c()!="$XSM":
+    raise Exception("%s is not an XSM file"%abspath(file_xsm.file.name))
   print file_xsm.read_int()
   offset_root = file_xsm.read_int()
   root = Node(offset_root)
-  root.load(file_xsm)
-  print root
-  depth_first_search(file_xsm,[root])
-##  for node in root.children:
-##    node.load(file_xsm)
-##    print node
-##    if node.type in [-1,0]:
-##      children = node.children
-##      for child in children:
-##        child.load(file_xsm)
-##        print child
-##        if child.type in [-1,0]:
-##          for grand_child in child.children:
-##            grand_child.load(file_xsm)
-##            print grand_child
-  #print file_xsm.read_4c(),"|"
-  #print file_xsm.read_int(), "|"
-  #print file_xsm.read_int(), "|"
-  #print file_xsm.read_int(), "|"
-  #print file_xsm.read_int(), "|"
-  #print file_xsm.read_12c(),"|"
-  #print file_xsm.read_ints(30), "|"
-  #print file_xsm.read_ints(30), "|"
-  #print file_xsm.read_ints(30), "|"
-  #print file_xsm.read_12c(), "|"
-  #print file_xsm.read_12c(), "|"
-  #print file_xsm.read_12c(), "|"
-  #file_xsm.seek(191)
-  #print file_xsm.read_12c(),"|"
-  #file_xsm.seek(194)
-  #print file_xsm.read_4c(),"|"
-  #file_xsm.seek(8766)
-  #print file_xsm.read_4c(),"|"
+  elementList = []
+  file_xsm.depth_first_search([root])
+  file_xsm.close()
+  return elementList
+
+if __name__=="__main__":
+  file_path = "../../example/XsmMultiCompoV4"
+  file_xsm = FileXsm(file_path)
+  if file_xsm.read_4c()!="$XSM":
+    raise Exception("%s is not an XSM file"%abspath(file_xsm.file.name))
+  print file_xsm.read_int()
+  offset_root = file_xsm.read_int()
+  root = Node(offset_root)
+  node_generator = file_xsm.depth_first_search([root])
+  for node in node_generator:
+    print node
+  file_xsm.close()
 
